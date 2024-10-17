982ff257cdd173d336b1a72b9cf47003e3d4b12f
NxKuma <neilisraelko01@gmail.com>
2024-10-18 01:48:06 +0800

    Updated Srcipts

--- iscs-rpg/Script/SuperScript/Entity.gd
+++ iscs-rpg/Script/SuperScript/Entity.gd
@@ -1,8 +1,9 @@
 extends Resource
 class_name Entity
 
+#Editable in the Inspector
 @export var name : String
-@export var health : int
+@export var health : float
 @export var mana: int
 @export var damage: float
 @export var skill_list: Array[Skill] = []
@@ -11,25 +12,34 @@ class_name Entity
 @export var weakness: Element
 @export var speed: int 
 
-
+#Hidden in the Inspector
 enum Element{
 	Fire,
 	Water,
 	Earth,
 	Wind,
-	Ligh,
+	Light,
 	Dark
 }
 
+var max_health: float = health
+var is_stunned:bool = false
+var is_dead: bool = false
+var crit_chance: float = 0.5
+var crit_multiplier: float = 1.5
+
+
+# ---Return Functions---
+
 #Take Physical Damage
-func take_damage(damage_dealt:int) -> int:
+func take_damage(damage_dealt:float) -> float:
 	health -= damage_dealt
 	#Return the damaged health
 	return health
 
 #Take Skill Damage
-func take_skill_damage(skill_recieved:Skill) -> int:
-	var initial_health:int = health
+func take_skill_damage(skill_recieved:Skill) -> float:
+	var initial_health:float = health
 	
 	#Check if the Skill Element is aligned with the Element of the Entity
 	#Based on this the damage will either weaken or strengthen
@@ -40,4 +50,18 @@ func take_skill_damage(skill_recieved:Skill) -> int:
 	
 	#Return the calculated health
 	return initial_health
-	
+
+
+# --- Non Return Functions---
+
+#Stun the Entity
+func stun():
+	is_stunned = true
+
+#Revive the Entity
+func revive():
+	if !is_dead:
+		return
+	else:
+		health = max_health
+		is_dead = false
--- iscs-rpg/Script/SuperScript/Skill.gd
+++ iscs-rpg/Script/SuperScript/Skill.gd
@@ -1,19 +1,20 @@
 extends Resource
 class_name Skill
 
+#Editable in the Inspector
 @export var skill_name: String
 @export_multiline var skill_description: String
 @export_enum("Attack", "Support") var skill_type: String
 @export_enum("Passive", "Active") var skill_mode: String
-@export var skill_element: String
+@export var skill_element: Element
 @export var skill_damage: float
-@export var isPassive: bool
 
+#Hidden in the Inspector
 enum Element{
 	Fire,
 	Water,
 	Earth,
 	Wind,
-	Ligh,
+	Light,
 	Dark
 }
