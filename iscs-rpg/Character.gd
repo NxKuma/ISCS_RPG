fdf23bda0dbec63f55af371fbf9a51ec25067669
NxKuma <neilisraelko01@gmail.com>
2024-10-22 11:34:13 +0800

    Implemented Outline
    Still Needs Fixing:
    Needs to only outline when Gamestate = Target

--- iscs-rpg/Character.gd
+++ iscs-rpg/Character.gd
@@ -11,26 +11,27 @@ var dissolve_shader:Shader = preload("res://Material/disolver.tres")
 var outline_shader:Shader = preload("res://Material/outline.tres")
 var current_shader:Shader
 
-var material_checks: Array[bool] = [true, false, false]
+var shader_checks: Array[bool] = [false, false, false]
 
-func align_material() -> void:
-	if material_checks[0]:
+func align_shader(shader_number: int) -> void:
+	if shader_number == 0:
 		current_shader = blink_shader
-	elif material_checks[1]:
+	elif shader_number == 1:
 		current_shader = dissolve_shader
-	elif material_checks[2]:
+	elif shader_number == 2:
 		current_shader = outline_shader
+	self.material.set_shader(current_shader)
 
-func reset_material(animation_number: int) -> void:
-	for check in material_checks:
-		check = false
-	material_checks[animation_number] = true
-	align_material()
+func reset_shader(animation_number: int) -> void:
+	for x in range(0,2):
+		shader_checks[x] = false
+	shader_checks[animation_number] = true
+	align_shader(animation_number)
 	
 
 func _ready() -> void:
 	player_name.text = stats.entity_name
-	#if material.resource_name == "Blink":
+	reset_shader(0)
 	
 func _process(delta: float) -> void:
 	health.text = "Health: " + str(stats.health)
@@ -38,9 +39,14 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_mouse_entered() -> void:
-	reset_material(3)
+	reset_shader(2)
+	self.material.set_shader_parameter("shader_parameter/Width",3)
+	print(self.shader_checks)
+	print(self.player_name.text)
 	
 
 
 func _on_area_2d_mouse_exited() -> void:
-	pass # Replace with function body.
+	reset_shader(0)
+	self.material.set_shader_parameter("shader_parameter/Width",0)
+	print("Goodbye")
